855f3a1f9af4620ab9fc83d8e6806c43256cef60
Kukarazi <soultrimmer@gmail.com>
2022-09-19 18:18:50 +0300

    Draw 🐝, draw 🌼, draw 🟩

--- script.js
+++ script.js
@@ -1,34 +1,83 @@
 /** @type {HTMLCanvasElement} */
 let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
 //
-const N = 7;
+const N = 10;
 const GRASS = 0;
-const WALL = 1;
-const BEE = 18;
+const ROCK = 1;
+const BEE = 2;
+const FLOWER = 3;
 const square = canvas.width / N;
 let field = new Array(N);
-//
+// ================================================
 document.addEventListener("DOMContentLoaded", function () {
   generateMaze();
-  console.log(field);
-  drawSquare();
-  drawLines();
+  drawMap();
+  drawGrid();
 });
-//
+// ================================================
 function generateMaze() {
+  function placeFlower() {
+    while (true) {
+      let x = getRandomInt(0, N - 1);
+      let y = getRandomInt(0, N - 1);
+      if (field[y][x] != BEE) {
+        field[y][x] = FLOWER;
+        break;
+      }
+    }
+  }
   for (let i = 0; i < N; i++) {
     field[i] = new Array(N);
   }
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      field[i][j] = getRandomInt(0, 1);
+      field[i][j] = getRandomInt(0, 99);
+      if (field[i][j] < 20) {
+        field[i][j] = ROCK;
+      } else if (field[i][j] >= 20) {
+        field[i][j] = GRASS;
+      }
     }
   }
   field[getRandomInt(0, N - 1)][getRandomInt(0, N - 1)] = BEE;
+  Array.from({ length: 3 }, (_x, _y) => {
+    placeFlower();
+  });
 }
-//
-function drawLines() {
-  let context = canvas.getContext("2d");
+// ================================================
+function drawMap() {
+  context.beginPath();
+  context.strokeStyle = "#ffffff";
+  context.font = "30px serif";
+  for (let y = 0; y < N; y++) {
+    for (let x = 0; x < N; x++) {
+      switch (field[y][x]) {
+        case ROCK:
+          context.fillStyle = "#adadad";
+          context.fillRect(square * x, square * y, square, square);
+          break;
+        case GRASS:
+          context.fillStyle = "#00ff00";
+          context.fillRect(square * x, square * y, square, square);
+          break;
+        case BEE:
+          context.fillStyle = "#00ff00";
+          context.fillRect(square * x, square * y, square, square);
+          context.fillText("🐝", square * x + 5, square * y + 40);
+          break;
+        case FLOWER:
+          context.fillStyle = "#00ff00";
+          context.fillRect(square * x, square * y, square, square);
+          context.fillText("🌼", square * x + 5, square * y + 40);
+          break;
+      }
+      context.stroke();
+    }
+  }
+}
+// ================================================
+function drawGrid() {
   context.beginPath();
   context.strokeStyle = "#ffffff";
   context.fillStyle = "coral";
@@ -46,40 +95,9 @@ function drawLines() {
   //
   context.stroke();
 }
-//
-function drawSquare() {
-  let context = canvas.getContext("2d");
-  context.beginPath();
-  context.fillStyle = "#aaaa00";
-  context.strokeStyle = "#ffffff";
-  for (let y = 0; y < N; y++) {
-    for (let x = 0; x < N; x++) {
-      if (field[y][x] === WALL) {
-        context.fillRect(square * x, square * y, square, square);
-      } else if (field[y][x] === BEE) {
-        // /
-        context.moveTo(square * (x + 1), square * y);
-        context.lineTo(square * x, square * (y + 1));
-        // \
-        context.moveTo(square * x, square * y); //
-        context.lineTo(square * (x + 1), square * (y + 1)); //
-        //
-        context.stroke();
-      }
-    }
-  }
-}
-//
+// ===============================================
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//
-//
-//
-let array = [1, 2, 3, 4, 5];
-array[0] = [0, 0, 0];
-array[1] = [0, 0, 0];
-array[2] = [0, 0, 0];
-console.log(array);
